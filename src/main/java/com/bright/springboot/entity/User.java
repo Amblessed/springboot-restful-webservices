0a4cdd92136caca8bb2ce222631d114c8543b9de
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2023-04-03 11:04:47 +0200

    Initial commit

---
+++ src/main/java/com/bright/springboot/entity/User.java
@@ -0,0 +1,11 @@
+package com.bright.springboot.entity;
+
+/*
+* @Project Name: springboot-restful-webservices
+* @Author: Okechukwu Bright Onwumere
+* @Created: 03/04/2023
+*/
+
+
+public class User {
+}
